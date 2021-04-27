feea12ffa3080b18357051e4f4f4ddfcc91a5bbd
Alit <alitwarasto@gmail.com>
2021-04-28 00:26:06 +0700

    pake mobirise konvert wp page

--- functions.php
+++ functions.php
@@ -10,36 +10,58 @@ $themeurl = get_bloginfo('template_url');
 require get_template_directory().'/inc/md/Mobile_Detect.php';*/
 
 function load_stylesheets(){
-   /* wp_register_style('bootstrap', get_template_directory_uri(). '/assets/css/bootstrap.min.css', '',4.0,'all');
-    wp_register_style('fontawesome', get_template_directory_uri(). '/assets/fa/css/fontawesome.min.css', '',5.1,'all');
-    wp_register_style('swiper', get_template_directory_uri(). '/assets/css/swiper-bundle.min.css', '',3.0,'all');
+   /**/ 
+		wp_register_style('mobiriseicon', get_template_directory_uri(). '/assets/web/assets/mobirise-icons2/mobirise2.css', '',2.0,'all');
+		wp_register_style('tether', get_template_directory_uri(). '/assets/tether/tether.min.css', '',1.0,'all');
+		wp_register_style('bootstrap', get_template_directory_uri(). '/assets/bootstrap/css/bootstrap.min.css', '',4.0,'all');
+		wp_register_style('bootstrapgrid', get_template_directory_uri(). '/assets/bootstrap/css/bootstrap-grid.min.css', '',4.0,'all');
+		wp_register_style('bootstrapreboot', get_template_directory_uri(). '/assets/bootstrap/css/bootstrap-reboot.min.css', '',4.0,'all');
+		wp_register_style('dropdown', get_template_directory_uri(). '/assets/dropdown/css/style.css', '',5.1,'all');
+		wp_register_style('socicon', get_template_directory_uri(). '/assets/socicon/css/styles.css', '',3.0,'all');
+		wp_register_style('themestyle', get_template_directory_uri(). '/assets/theme/css/style.css', '',3.0,'all');
+		wp_register_style('addstyle', get_template_directory_uri(). '/assets/mobirise/css/mbr-additional.css', '',3.0,'all');
+		wp_register_style('appcss', get_template_directory_uri(). '/assets/css/app.css', '',0.1,'all');
+    
+    wp_enqueue_style('mobiriseicon');
+    wp_enqueue_style('tether');
     wp_enqueue_style('bootstrap');
-    wp_enqueue_style('swiper');
-    wp_enqueue_style('fontawesome');
-    $detect = new Mobile_Detect; #mobile detect#
-    if ( $detect->isMobile() && !$detect->isTablet() ){
-        wp_register_style('appcssm', get_template_directory_uri(). '/assets/css/appm.css', '',0.1,'all');
-        wp_enqueue_style('appcssm');
-    }else{*/
-        wp_register_style('appcss', get_template_directory_uri(). '/assets/css/app.css', '',0.1,'all');
-        wp_enqueue_style('appcss');
- /*   }*/
+    wp_enqueue_style('bootstrapgrid');
+    wp_enqueue_style('bootstrapreboot');
+    wp_enqueue_style('dropdown');
+    wp_enqueue_style('socicon');
+    wp_enqueue_style('themestyle');
+    wp_enqueue_style('addstyle');
+    wp_enqueue_style('appcss');
 }
 
 add_action('wp_enqueue_scripts','load_stylesheets',1);
-/*function load_javascript(){
-    wp_register_script('jquery351', get_template_directory_uri(). '/assets/js/jquery351.js','', 3.5, true);
-    wp_register_script('bootstrap', get_template_directory_uri(). '/assets/js/bootstrap.min.js','', 4.0, true);
-    wp_register_script('fontawesome', get_template_directory_uri(). '/assets/fa/js/fontawesome.min.js','', 4.0, true);
-    wp_register_script('swiper', get_template_directory_uri(). '/assets/js/swiper-bundle.min.js','jquery', 3, true);
+/**/
+function load_javascript(){
+    wp_register_script('jquery351', get_template_directory_uri(). '/assets/web/assets/jquery/jquery.min.js','', 3.5, true);
+    wp_register_script('popper', get_template_directory_uri(). '/assets/popper/popper.min.js','', 4.0, true);
+    wp_register_script('tether', get_template_directory_uri(). '/assets/tether/tether.min.js','jquery', 3, true);
+    wp_register_script('bootstrap', get_template_directory_uri(). '/assets/bootstrap/js/bootstrap.min.js','', 4.0, true);
+    wp_register_script('scroll', get_template_directory_uri(). '/assets/smoothscroll/smooth-scroll.js','', 4.0, true);
+    wp_register_script('parallax', get_template_directory_uri(). '/assets/parallax/jarallax.min.js','', 4.0, true);
+    wp_register_script('dropdown', get_template_directory_uri(). '/assets/dropdown/js/nav-dropdown.js','', 4.0, true);
+    wp_register_script('dropdownNav', get_template_directory_uri(). '/assets/dropdown/js/navbar-dropdown.js','', 4.0, true);
+    wp_register_script('touchswipe', get_template_directory_uri(). '/assets/touchswipe/jquery.touch-swipe.min.js','', 4.0, true);
+    wp_register_script('theme', get_template_directory_uri(). '/assets/theme/js/script.js','', 4.0, true);
     wp_register_script('appjs', get_template_directory_uri(). '/assets/js/app.js','',0.1, true);
+
     wp_enqueue_script('jquery351');
+    wp_enqueue_script('popper');
+    wp_enqueue_script('tether');
     wp_enqueue_script('bootstrap');
-    wp_enqueue_script('fontawesome');
-    wp_enqueue_script('swiper');
+    wp_enqueue_script('scroll');
+    wp_enqueue_script('parallax');
+    wp_enqueue_script('dropdown');
+    wp_enqueue_script('dropdownNav');
+    wp_enqueue_script('touchswipe');
+    wp_enqueue_script('theme');
     wp_enqueue_script('appjs');
 }
-add_action('wp_enqueue_scripts', 'load_javascript');*/
+add_action('wp_enqueue_scripts', 'load_javascript');
 
 function fle_logo() {
     $defaults = array(
--- header.php
+++ header.php
@@ -5,11 +5,45 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $sitedesc." | ".$sitename ?></title>
-  <script src="https://kit.fontawesome.com/97cf4b0714.js"></script>
-
+  <meta name="description" content="">
   <?php 
  	wp_head();
 	get_header();
 	?>
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap">
+  </noscript>
+    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
 </head>
 <body>
+  <section class="menu cid-s48OLK6784" once="menu" id="menu1-h">
+    
+    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+        <div class="container">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-icon-121x121.png" alt="Rindu Ronde" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-black display-7" href="#">Rindu Ronde</a></span>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-black display-4" href="#">Kemitraan</a></li>
+                    <li class="nav-item"><a class="nav-link link text-black display-4" href="#">Hubungi Kami</a></li></ul>
+                
+                
+            </div>
+        </div>
+    </nav>
+
+</section>
