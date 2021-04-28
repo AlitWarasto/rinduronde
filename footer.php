5a590b15d14d396828c22cd60a347f3bd1ec7913
Alit <alitwarasto@gmail.com>
2021-04-28 22:56:01 +0700

    pake mobirise edit sitik

--- footer.php
+++ footer.php
@@ -1,68 +1,47 @@
 		<section class="footer3 cid-s48P1Icc8J" once="footers" id="footer3-i">
-
-    
-
-    
-
-    <div class="container">
+      <div class="container">
         <div class="media-container-row align-center mbr-white">
-            <div class="row row-links">
-                <ul class="foot-menu">
-                    
-                    
-                    
-                    
-                    
-                <li class="foot-menu-item mbr-fonts-style display-7">Hubungi Kami</li><li class="foot-menu-item mbr-fonts-style display-7">Lokasi</li><li class="foot-menu-item mbr-fonts-style display-7">rinduronde.com</li></ul>
+          <div class="row row-links">
+            <ul class="foot-menu">
+              <li class="foot-menu-item mbr-fonts-style display-7"><a style="color: aliceblue;" href="https://wa.me/6289607916051">Hubungi Kami</a></li>
+              <li class="foot-menu-item mbr-fonts-style display-7"><a style="color: aliceblue;" href="#">Lokasi</a></li>
+              <li class="foot-menu-item mbr-fonts-style display-7"><a style="color: aliceblue;" href="#">rinduronde.com</a></li>
+            </ul>
+          </div>
+          <div class="row social-row">
+            <div class="social-list align-right pb-2">
+              <div class="soc-item">
+                <a href="#" target="_blank"> <span class="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span> </a>
+              </div>
+              <div class="soc-item">
+                <a href="#" target="_blank"> <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span> </a>
+              </div>
+              <div class="soc-item">
+                <a href="#" target="_blank"> <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span> </a>
+              </div>
+              <div class="soc-item">
+                <a href="#" target="_blank"> <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span> </a>
+              </div>
             </div>
-            <div class="row social-row">
-                <div class="social-list align-right pb-2">
-                    
-                    
-                    
-                    
-                    
-                    
-                <div class="soc-item">
-                        <a href="https://twitter.com/mobirise" target="_blank">
-                            <span class="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
-                        </a>
-                    </div><div class="soc-item">
-                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
-                            <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
-                        </a>
-                    </div><div class="soc-item">
-                        <a href="https://www.youtube.com/c/mobirise" target="_blank">
-                            <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
-                        </a>
-                    </div><div class="soc-item">
-                        <a href="https://instagram.com/mobirise" target="_blank">
-                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
-                        </a>
-                    </div></div>
-            </div>
-            <div class="row row-copirayt">
-                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
-                    © Copyright 2021 rinduronde. All Rights Reserved.
-                </p>
-            </div>
+          </div>
+          <div class="row row-copirayt"><p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">© Copyright 2021 rinduronde. All Rights Reserved.</p></div>
         </div>
-    </div>
-</section>
-<section style="background-color: #fff; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, sans-serif; color:#aaa; font-size:12px; padding: 0; align-items: center; display: none;">
-    <a href="" style="flex: 1 1; height: 3rem; padding-left: 1rem;"></a>
-    <p style="flex: 0 0 auto; margin:0; padding-right:1rem;">Page was <a href="https://mobirise.site/h" style="color:#aaa;">built with</a> love</p>
-</section>
-<script src="assets/web/assets/jquery/jquery.min.js"></script>
-<script src="assets/popper/popper.min.js"></script>
-<script src="assets/tether/tether.min.js"></script>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/smoothscroll/smooth-scroll.js"></script>
-<script src="assets/parallax/jarallax.min.js"></script>
-<script src="assets/dropdown/js/nav-dropdown.js"></script>
-<script src="assets/dropdown/js/navbar-dropdown.js"></script>
-<script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
-<script src="assets/theme/js/script.js"></script>  
+      </div>
+    </section>
+    <section style="background-color: #fff; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, sans-serif; color: #aaa; font-size: 12px; padding: 0; align-items: center; display: none;">
+      <a href="https://mobirise.site/g" style="flex: 1 1; height: 3rem; padding-left: 1rem;"></a>
+      <p style="flex: 0 0 auto; margin: 0; padding-right: 1rem;"><a href="https://mobirise.site" style="color: #aaa;">Mobirise</a> html site maker</p>
+    </section>
+    <script src="assets/web/assets/jquery/jquery.min.js"></script>
+    <script src="assets/popper/popper.min.js"></script>
+    <script src="assets/tether/tether.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/parallax/jarallax.min.js"></script>
+    <script src="assets/dropdown/js/nav-dropdown.js"></script>
+    <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+    <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+    <script src="assets/theme/js/script.js"></script>
    	
   	<?php wp_footer(); ?>
 	</body>
--- header.php
+++ header.php
@@ -10,40 +10,30 @@
  	wp_head();
 	get_header();
 	?>
-  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
-  <noscript>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap">
-  </noscript>
-    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
-</head>
-<body>
-  <section class="menu cid-s48OLK6784" once="menu" id="menu1-h">
-    
-    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'" />
+    <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" /> </noscript>
+    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css" />
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css" />
+  </head>
+  <body>
+    <section class="menu cid-s48OLK6784" once="menu" id="menu1-h">
+      <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
         <div class="container">
-            <div class="navbar-brand">
-                <span class="navbar-logo">
-                    <a href="">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-icon-121x121.png" alt="Rindu Ronde" style="height: 3.8rem;">
-                    </a>
-                </span>
-                <span class="navbar-caption-wrap"><a class="navbar-caption text-black display-7" href="#">Rindu Ronde</a></span>
-            </div>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <div class="hamburger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-black display-4" href="#">Kemitraan</a></li>
-                    <li class="nav-item"><a class="nav-link link text-black display-4" href="#">Hubungi Kami</a></li></ul>
-                
-                
-            </div>
+          <div class="navbar-brand">
+            <span class="navbar-logo">
+              <a href="<?php echo $siteurl; ?>"> <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-icon-121x121.png" alt="Rindu Ronde" style="height: 3.8rem;" /> </a>
+            </span>
+            <span class="navbar-caption-wrap"><a class="navbar-caption text-black display-7" href="<?php echo $siteurl; ?>">Rindu Ronde</a></span>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger"><span></span> <span></span> <span></span> <span></span></div>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+              <li class="nav-item"><a class="nav-link link text-black display-4" href="https://wa.me/6289607916051">Kemitraan</a></li>
+              <li class="nav-item"><a class="nav-link link text-black display-4" href="https://wa.me/6289607916051">Hubungi Kami</a></li>
+            </ul>
+          </div>
         </div>
-    </nav>
-
-</section>
+      </nav>
+    </section>
